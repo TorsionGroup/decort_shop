6f2061a68ca7bb92f3f691194748602c96b84932
AlexGr33n <greentr33@gmail.com>
2021-02-08 21:45:23 +0200

    ++ csv to sql

--- decort_shop/shop/api/csv_to_pgsql.py
+++ decort_shop/shop/api/csv_to_pgsql.py
@@ -10,7 +10,7 @@ conn = psycopg2.connect(
 )
 cur = conn.cursor()
 
-t_sql = '''CREATE TEMPORARY TABLE shop_currency_buffer (
+t_sql = '''CREATE TABLE shop_currency_buffer (
     code character varying(250),
     name character varying(250),
     title character varying(250),
@@ -23,9 +23,21 @@ conn.commit()
 
 with open('download/currencies.csv', 'r+', encoding='utf-8') as file:
     cur.copy_from(file, 'shop_currency_buffer', columns=('source_id', 'code', 'name', 'title', 'rate', 'mult'), sep='|')
+
 conn.commit()
 
+copy_sql = '''UPDATE shop_currency c
+    SET
+       code  = b.code,
+       name  = b.name,
+       title = b.title,
+       rate  = b.rate,
+       mult  = b.mult
+    FROM shop_currency_buffer b
+    WHERE c.source_id = b.source_id;'''
 
+cur.execute(copy_sql)
+conn.commit()
 
 conn.close()
 print('Connection closed')
