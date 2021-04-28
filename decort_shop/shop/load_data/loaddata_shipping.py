64d89a9b5fccfb22e61ec06cb1e94df20ad52c80
AlexGr33n <greentr33@gmail.com>
2021-04-28 14:03:19 +0300

    add load data novaposhta

--- decort_shop/shop/load_data/loaddata_shipping.py
+++ decort_shop/shop/load_data/loaddata_shipping.py
@@ -55,12 +55,63 @@ class LoadDataShipping:
         file.write(str(data.decode('utf-8')))
         file.close()
 
+        cur = self.conn.cursor()
+
+        t_sql = '''CREATE TEMP TABLE shipping_novaposhtaregion_buffer (
+            source character varying(300),
+            name character varying(300) );'''
+        cur.execute(t_sql)
+        self.conn.commit()
+
+        with open('cache/novaposhta_regions.csv', 'r', encoding='utf-8') as file:
+            cur.copy_from(file, 'shipping_novaposhtaregion_buffer',
+                          columns=('source', 'name'), sep='|')
+        self.conn.commit()
+
+        copy_sql = '''UPDATE shipping_novaposhtaregion r
+            SET
+                name = b.name       
+            FROM shipping_novaposhtaregion_buffer b
+            WHERE r.source = b.source;'''
+        cur.execute(copy_sql)
+        self.conn.commit()
+
     def load_novaposhta_cities(self):
         novaposhta_cities = self.client.service.GetData('novaposhta_cities')
         data = base64.b64decode(novaposhta_cities)
         file = open('cache/novaposhta_cities.csv', 'w', newline='', encoding='utf-8')
         file.write(str(data.decode('utf-8')))
         file.close()
+
+        cur = self.conn.cursor()
+
+        t_sql = '''CREATE TEMP TABLE shipping_novaposhtacity_buffer (
+            source character varying(300),
+            source_region character varying(300),
+            name character varying(300) );'''
+        cur.execute(t_sql)
+        self.conn.commit()
+
+        with open('cache/novaposhta_cities.csv', 'r', encoding='utf-8') as file:
+            cur.copy_from(file, 'shipping_novaposhtacity_buffer',
+                          columns=('source', 'source_region', 'name'), sep='|')
+        self.conn.commit()
+
+        copy_sql = '''UPDATE shipping_novaposhtacity c
+            SET
+                source_region = b.source_region,
+                name = b.name       
+            FROM shipping_novaposhtacity_buffer b
+            WHERE c.source = b.source;'''
+        cur.execute(copy_sql)
+        self.conn.commit()
+
+        upd_sql = '''UPDATE shipping_novaposhtacity c
+            SET region_id = r.id                               
+            FROM shipping_novaposhtaregion r
+            WHERE c.source_region = r.source;'''
+        cur.execute(upd_sql)
+        self.conn.commit()
 
     def load_novaposhta_branches(self):
         novaposhta_branches = self.client.service.GetData('novaposhta_branches')
@@ -69,12 +120,81 @@ class LoadDataShipping:
         file.write(str(data.decode('utf-8')))
         file.close()
 
+        cur = self.conn.cursor()
+
+        t_sql = '''CREATE TEMP TABLE shipping_novaposhtabranche_buffer (
+            source character varying(300),
+            source_city character varying(300),
+            name character varying(300),
+            branche_type character varying(300),
+            max_weight_place integer,
+            max_weight integer );'''
+        cur.execute(t_sql)
+        self.conn.commit()
+
+        with open('cache/novaposhta_branches.csv', 'r', encoding='utf-8') as file:
+            cur.copy_from(file, 'shipping_novaposhtabranche_buffer',
+                          columns=('source', 'source_city', 'name', 'branche_type', 'max_weight_place', 'max_weight'),
+                          sep='|')
+        self.conn.commit()
+
+        copy_sql = '''UPDATE shipping_novaposhtabranche c
+            SET
+                source_city = b.source_city,
+                name = b.name,
+                branche_type = b.branche_type,
+                max_weight_place = b.max_weight_place,
+                max_weight = b.max_weight       
+            FROM shipping_novaposhtabranche_buffer b
+            WHERE c.source = b.source;'''
+        cur.execute(copy_sql)
+        self.conn.commit()
+
+        upd_sql = '''UPDATE shipping_novaposhtabranche b
+            SET city_id = c.id                               
+            FROM shipping_novaposhtacity c
+            WHERE b.source_city = c.source;'''
+        cur.execute(upd_sql)
+        self.conn.commit()
+
     def load_novaposhta_streetes(self):
         novaposhta_streetes = self.client.service.GetData('novaposhta_streetes')
         data = base64.b64decode(novaposhta_streetes)
         file = open('cache/novaposhta_streetes.csv', 'w', newline='', encoding='utf-8')
         file.write(str(data.decode('utf-8')))
         file.close()
+
+        cur = self.conn.cursor()
+
+        t_sql = '''CREATE TEMP TABLE shipping_novaposhtastreet_buffer (
+            source character varying(300),
+            source_city character varying(300),
+            name character varying(300),
+            street_type character varying(300) );'''
+        cur.execute(t_sql)
+        self.conn.commit()
+
+        with open('cache/novaposhta_streetes.csv', 'r', encoding='utf-8') as file:
+            cur.copy_from(file, 'shipping_novaposhtastreet_buffer',
+                          columns=('source', 'source_city', 'name', 'street_type'), sep='|')
+        self.conn.commit()
+
+        copy_sql = '''UPDATE shipping_novaposhtastreet c
+            SET
+                source_city = b.source_city,
+                name = b.name,
+                street_type = b.street_type       
+            FROM shipping_novaposhtastreet_buffer b
+            WHERE c.source = b.source;'''
+        cur.execute(copy_sql)
+        self.conn.commit()
+
+        upd_sql = '''UPDATE shipping_novaposhtastreet s
+            SET city_id = c.id                               
+            FROM shipping_novaposhtacity c
+            WHERE s.source_city = c.source;'''
+        cur.execute(upd_sql)
+        self.conn.commit()
 
 
 LoadDataShipping = LoadDataShipping()
