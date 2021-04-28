ca842368e78c3b90caca1f7d04866fa4d7f63086
AlexGr33n <greentr33@gmail.com>
2021-04-28 11:53:02 +0300

    add load data tecdoc

--- decort_shop/shop/load_data/loaddata_tecdoc.py
+++ decort_shop/shop/load_data/loaddata_tecdoc.py
@@ -28,12 +28,98 @@ class LoadDataTecdoc:
         file.write(str(data.decode('utf-8')))
         file.close()
 
+        cur = self.conn.cursor()
+
+        t_sql = '''CREATE TEMP TABLE tecdoc_manufacturer_buffer (
+                source character varying(300),
+                name character varying(300), 
+                manufacturer_tecdoc_id character varying(300),
+                canbedisplayed boolean,
+                ispassengercar boolean,
+                iscommercialvehicle boolean,
+                ismotorbike boolean,
+                isengine boolean,
+                isaxle boolean );'''
+        cur.execute(t_sql)
+        self.conn.commit()
+
+        with open('cache/tecdoc_manufacturer.csv', 'r', encoding='utf-8') as file:
+            cur.copy_from(file, 'tecdoc_manufacturer_buffer',
+                          columns=('source', 'name', 'manufacturer_tecdoc_id', 'canbedisplayed', 'ispassengercar',
+                                   'iscommercialvehicle', 'ismotorbike', 'isengine', 'isaxle'), sep='|')
+        self.conn.commit()
+
+        copy_sql = '''UPDATE tecdoc_manufacturer t
+                SET
+                    name = b.name,
+                    manufacturer_tecdoc_id = b.manufacturer_tecdoc_id, 
+                    canbedisplayed = b.canbedisplayed,
+                    ispassengercar = b.ispassengercar,
+                    iscommercialvehicle = b.iscommercialvehicle,
+                    ismotorbike = b.ismotorbike,
+                    isengine = b.isengine,                    
+                    isaxle = b.isaxle            
+                FROM tecdoc_manufacturer_buffer b
+                WHERE t.source = b.source;'''
+        cur.execute(copy_sql)
+        self.conn.commit()
+
     def load_tecdoc_manufacturer_model(self):
         tecdoc_manufacturer_model = self.client.service.GetData('tecdoc_manufacturer_model')
         data = base64.b64decode(tecdoc_manufacturer_model)
         file = open('cache/tecdoc_manufacturer_model.csv', 'w', newline='', encoding='utf-8')
         file.write(str(data.decode('utf-8')))
         file.close()
+
+        cur = self.conn.cursor()
+
+        t_sql = '''CREATE TEMP TABLE tecdoc_manufacturermodel_buffer (
+                source character varying(300),
+                source_manufacturer character varying(300),
+                name character varying(300),
+                constructioninterval character varying(300), 
+                model_tecdoc_id character varying(300),  
+                manufacturer_tecdoc_id character varying(300),
+                canbedisplayed boolean,
+                ispassengercar boolean,
+                iscommercialvehicle boolean,
+                ismotorbike boolean,
+                isengine boolean,
+                isaxle boolean );'''
+        cur.execute(t_sql)
+        self.conn.commit()
+
+        with open('cache/tecdoc_manufacturer_model.csv', 'r', encoding='utf-8') as file:
+            cur.copy_from(file, 'tecdoc_manufacturermodel_buffer',
+                          columns=('source', 'source_manufacturer', 'name', 'constructioninterval', 'model_tecdoc_id',
+                                   'manufacturer_tecdoc_id', 'canbedisplayed', 'ispassengercar', 'iscommercialvehicle',
+                                   'ismotorbike', 'isengine', 'isaxle'), sep='|')
+        self.conn.commit()
+
+        copy_sql = '''UPDATE tecdoc_manufacturermodel t
+                SET
+                    source_manufacturer = b.source_manufacturer,
+                    name = b.name,
+                    constructioninterval = b.constructioninterval,
+                    model_tecdoc_id = b.model_tecdoc_id,
+                    manufacturer_tecdoc_id = b.manufacturer_tecdoc_id, 
+                    canbedisplayed = b.canbedisplayed,
+                    ispassengercar = b.ispassengercar,
+                    iscommercialvehicle = b.iscommercialvehicle,
+                    ismotorbike = b.ismotorbike,
+                    isengine = b.isengine,                    
+                    isaxle = b.isaxle            
+                FROM tecdoc_manufacturermodel_buffer b
+                WHERE t.source = b.source;'''
+        cur.execute(copy_sql)
+        self.conn.commit()
+
+        upd_sql = '''UPDATE tecdoc_manufacturermodel t
+            SET manufacturer_id = m.id                               
+            FROM tecdoc_manufacturer m
+            WHERE t.source_manufacturer = m.source;'''
+        cur.execute(upd_sql)
+        self.conn.commit()
 
 
 LoadDataTecdoc = LoadDataTecdoc()
