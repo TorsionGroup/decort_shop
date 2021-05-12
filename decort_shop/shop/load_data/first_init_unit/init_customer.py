7828d0329156bd877b15f04874fc5b2a6c605cdb
AlexGr33n <greentr33@gmail.com>
2021-05-12 13:46:21 +0300

    add -- users

--- decort_shop/shop/load_data/first_init_unit/init_customer.py
+++ decort_shop/shop/load_data/first_init_unit/init_customer.py
@@ -9,3 +9,33 @@ conn = psycopg2.connect(
 )
 cur = conn.cursor()
 
+with open('../cache/customer_discounts.csv', 'r', encoding='utf-8') as file:
+    cur.copy_from(file, 'customers_customerdiscount',
+                  columns=('source_id', 'brand', 'customer', 'agreement',
+                           'price_type', 'criteria_type', 'discount'), sep='|')
+conn.commit()
+print('Load Customer Discount')
+
+with open('../cache/customer_contacts.csv', 'r', encoding='utf-8') as file:
+    cur.copy_from(file, 'customers_customercontact',
+                  columns=('source', 'source_customer', 'name', 'email', 'phone', 'is_user', 'birthday'), sep='|')
+conn.commit()
+print('Load Customer Contact')
+
+with open('../cache/customer_points.csv', 'r', encoding='utf-8') as file:
+    cur.copy_from(file, 'customers_customerpoint', columns=('customer', 'source_id', 'name', 'add'), sep='|')
+conn.commit()
+print('Load Customer Point')
+
+with open('../cache/customer_agreements.csv', 'r', encoding='utf-8') as file:
+    cur.copy_from(file, 'customers_customeragreement',
+                  columns=('source_id', 'customer', 'currency', 'price_type',
+                           'code', 'name', 'number', 'discount', 'is_status', 'is_active'), sep='|')
+conn.commit()
+print('Load Customer Agreement')
+
+with open('../cache/balances.csv', 'r', encoding='utf-8') as file:
+    cur.copy_from(file, 'customers_balance',
+                  columns=('customer', 'agreement', 'currency', 'balance', 'past_due'), sep='|')
+conn.commit()
+print('Load Balance')
---
+++ decort_shop/shop/load_data/loaddata_users.py
@@ -0,0 +1,53 @@
+import base64
+import psycopg2
+from zeep import Client, Settings
+from requests import Session
+from requests.auth import HTTPBasicAuth
+from zeep.transports import Transport
+
+
+class LoadDataUsers:
+    def __init__(self):
+        session = Session()
+        session.auth = HTTPBasicAuth('Robot', 'Robot')
+        transport = Transport(session=session, timeout=600)
+        settings = Settings(xml_huge_tree=True)
+        self.client = Client('http://192.168.75.115:8005/live/ws/decort?wsdl', transport=transport, settings=settings)
+
+        self.conn = psycopg2.connect(
+            host="localhost",
+            port="5432",
+            database="decort_shop",
+            user="torsion_prog",
+            password="sdr%7ujK")
+
+    def load_users(self):
+        cur = self.conn.cursor()
+
+        copy_sql = '''UPDATE shop_account a
+            SET              
+                username = b.name,
+                email = b.email,
+                phone = b.phone,               
+                is_active = b.is_user,
+                date_of_birth = b.birthday                         
+            FROM customers_customercontact b
+            WHERE customer_id_id = b.customer_id_id;'''
+        cur.execute(copy_sql)
+        self.conn.commit()
+
+    def load_new_users(self):
+        cur = self.conn.cursor()
+
+        copy_sql = '''INSERT INTO shop_account (username, email, phone, is_active, date_of_birth, customer_id_id)
+            SELECT name, email, phone, is_user, birthday, customer_id_id                           
+            FROM customers_customercontact b
+            WHERE b.customer_id_id IS NOT NULL;'''
+        cur.execute(copy_sql)
+        self.conn.commit()
+
+
+LoadDataUsers = LoadDataUsers()
+LoadDataUsers.load_users()
+# LoadDataUsers.load_new_users()
+print('Load Data Users')
