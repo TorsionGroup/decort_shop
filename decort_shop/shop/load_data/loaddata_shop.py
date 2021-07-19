e478d06733b916574aae18ae634338f2ded781b1
AlexGr33n <greentr33@gmail.com>
2021-07-19 18:03:20 +0300

    catalog category +-

--- decort_shop/shop/load_data/loaddata_shop.py
+++ decort_shop/shop/load_data/loaddata_shop.py
@@ -428,8 +428,9 @@ class LoadDataShop:
         copy_sql = '''UPDATE shop_product p
             SET parent_category_id_id = c.id
             FROM shop_catalogcategory c
-            WHERE c.id IN (SELECT parent_id FROM shop_catalogcategory 
-            WHERE p.category = c.source_id);'''
+            WHERE c.id IN (SELECT parent_id FROM shop_catalogcategory
+            INNER JOIN shop_product p
+            ON p.category = c.source_id);'''
         cur.execute(copy_sql)
         self.conn.commit()
 
